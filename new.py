4c29f5b80a4a8331365ca0bd99807701fa97c66e
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2025-06-01 16:05:14 +0600

    minor changes in the new

--- new.py
+++ new.py
@@ -2,10 +2,9 @@ inputs = [1, 2, 3, 2.5]
 weights = [[0.91, 0.61, 0.7, 1.55], [0.74, 2.31, 1.7, 0.34], [2.1, 2, 0.7, 0.52]]
 bias = [2, 3, 0.5]
 
-temp = zip(weights, bias)
 # print(list(temp))
 layer_output = []
-for neuron_weights, neuron_biases in temp:
+for neuron_weights, neuron_biases in zip(weights, bias):
     neuron_output = 0
     for n_input, n_weight in zip(inputs, neuron_weights):
         neuron_output += n_input * neuron_output
