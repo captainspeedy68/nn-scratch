152a48dfbc10b3bb082373001d6c3c60024fb169
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2025-06-01 14:30:39 +0600

    initial commit

---
+++ .ipynb_checkpoints/scratch-checkpoint.py
@@ -0,0 +1,4 @@
+print("Hello world")
+for i in range(3,10):
+   print(i + 1)
+print("this is another try to see if waka time is picking up my code in jupyter lab")
---
+++ scratch.py
@@ -0,0 +1,18 @@
+
+# %%
+print("hellow world")
+for i in range(3,10):
+   print(i + 1)
+
+print("this is another try to see if waka time is picking up my ode in jupyter lab")
+
+# %%
+print("hellow this is from a cell")
+print("this is from the same cell but without a space")
+
+print("same cell but after a space")
+# %%
+print("this is another cell I dont know if will work or not tho so god knows what I am doing here")
+# %%
+
+
