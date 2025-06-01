3b76e06f69d8a27a18c1f001f59672e042f24ad3
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2025-06-01 17:27:27 +0600

    using classes to create layers

---
+++ using_classes.py
@@ -0,0 +1,18 @@
+import numpy as np
+np.random.seed(0)
+X = [[1, 2, 3, 2.5], [2.0, 1.0, -1.0, 2.0], [-1.5, 1.2, 2.7, 0.8]]
+
+class layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+#       multiplying with .10 gives us numbers closer to zero
+        self.weights = .10 *np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = layer_Dense(4, 5)
+layer2 = layer_Dense(5,2)
+
+layer1.forward(X)
+print(layer1.output)
